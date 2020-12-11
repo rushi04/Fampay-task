87ff4199411276e675659d32bb00b68db555786e
rushi04 <rushirocks2136@gmail.com>
2020-12-12 03:39:18 +0530

    Added Models

--- Fampay/Fetch_Video/models.py
+++ Fampay/Fetch_Video/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Video(models.Model):
+    class Meta:
+        ordering=['-publishedAt']
+
+    videoId = models.CharField(primary_key = True,max_length=150, blank=True)
+    title = models.TextField()
+    description = models.TextField()
+    publishedAt = models.DateTimeField(default=datetime.now, db_index=True, blank=True)
+    thumbnail = models.TextField(blank = True)
+    channelId = models.CharField(max_length = 100)
+
+Class Channel(models.Model):
+    channelId = models.CharField(db_index=True, max_length = 100)
+    channelTitle = models.TextField()
